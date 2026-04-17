a51ff0b2182ae6a83388fdcf8416d84a458840be
48-iq <ivn25ov@mail.ru>
2026-04-17 14:07:54 +0400

    fix

--- src/user/index.ts
+++ src/user/index.ts
@@ -2,3 +2,4 @@ export * from "./user-cursor-result.dto";
 export * from "./user-entity.dto";
 export * from "./user-result.dto";
 export * from "./user-update.dto";
+export * from "./user-create.dto";
