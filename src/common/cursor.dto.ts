31d6f2416fcc755d3c6b143ddcafc40b0756b4d5
48-iq <ivn25ov@mail.ru>
2026-04-17 13:08:32 +0400

    fix

--- src/common/cursor.dto.ts
+++ src/common/cursor.dto.ts
@@ -1,18 +1,6 @@
 export class CursorDto<T> {
   itemsLeft: number;
   data: T[];
-  nextItemTimestamp: string;
-  nextSkipItems: number;
-
-  constructor(args: {
-    itemsLeft: number;
-    data: T[];
-    nextItemTimestamp: string;
-    nextSkipItems: number;
-  }) {
-    this.itemsLeft = args.itemsLeft;
-    this.data = args.data;
-    this.nextItemTimestamp = args.nextItemTimestamp;
-    this.nextSkipItems = args.nextSkipItems;
-  }
+  startDate: string;
+  itemsSkipped: number;
 }
