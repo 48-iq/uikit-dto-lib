3aa345c59d9b3f25919998d85dc0d81903ae0c37
48-iq <ivn25ov@mail.ru>
2026-04-17 12:11:20 +0400

    fix

--- src/components/component-entity.dto.ts
+++ src/components/component-entity.dto.ts
@@ -1,6 +1,23 @@
 import { EntityDto } from "@/common/entity.dto";
 
 export class ComponentEntityDto extends EntityDto {
+  constructor(
+    args: {
+      id: string;
+      name: string;
+      username: string;
+      framework: string;
+      description: string;
+      createdAt: string;
+      updatedAt: string;
+    }
+  ){
+    super(args);
+    this.name = args.name;
+    this.username = args.username;
+    this.framework = args.framework;
+    this.description = args.description;
+  }
   name: string;
   username: string;
   framework: string;
