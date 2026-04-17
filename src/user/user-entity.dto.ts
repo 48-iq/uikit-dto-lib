0426e0f198dc227238489dd9a948c10e60147efd
48-iq <ivn25ov@mail.ru>
2026-04-17 14:17:16 +0400

    fix

--- src/user/user-entity.dto.ts
+++ src/user/user-entity.dto.ts
@@ -1,7 +1,6 @@
 import { EntityDto } from "../common/entity.dto";
 
 export class UserEntityDto extends EntityDto {
-  username: string;
   email: string;
 
   constructor(args: { 
@@ -9,11 +8,9 @@ export class UserEntityDto extends EntityDto {
       createdAt: string; 
       updatedAt: string; 
       email: string;
-      username: string
   }) {
     super(args);
     this.email = args.email;
-    this.username = args.username;
   }
 }
 
